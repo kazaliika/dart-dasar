1836edc0f02c67f2ba4d81a9783d721b3e1e6bd5
kazaliika <mavero811@gmail.com>
2024-03-18 21:32:31 +0700

    menambahkan nullable

--- fahrenheit-2-celcius.dart
+++ fahrenheit-2-celcius.dart
@@ -2,11 +2,13 @@ import 'dart:io';
 
 void main() {
   stdout.write("Masukkan suhu dalam Fahrenheit : ");
+
   var fahrenheit = num.parse(stdin.readLineSync()!);
+  if (fahrenheit != null) {
+    var celcius = (fahrenheit - 32) * 5 / 9;
 
-  var celcius = (fahrenheit - 32) * 5 / 9;
+    var hasil = "${num.parse(celcius.toStringAsFixed(2))}";
 
-  var hasil = "${num.parse(celcius.toStringAsFixed(2))}";
-
-  print("$fahrenheit derajat Fahrenheit = $hasil derajat celcius");
+    print("$fahrenheit derajat Fahrenheit = $hasil derajat celcius");
+  }
 }
